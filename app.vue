03235f181bf41360c404085031cc4503171784f4
Samuel Oechsler <samuel@oechsler.it>
2022-08-24 21:30:24 +0200

    Add placeholder content

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="w-full h-screen flex flex-col justify-center items-center">
+    <h1 class="text-4xl font-normal"><stong class="font-bold">oechsler</stong>.it</h1>
+    <p class="mt-4 py-2 px-6 border rounded-full">🚧 This page is work in progress</p>
   </div>
 </template>
